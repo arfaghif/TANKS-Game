9ced9bc13ad70eabc8d4c4c8dd9903372cab5a9f
arfaghif <52845090+arfaghif@users.noreply.github.com>
2021-03-26 18:03:30 +0700

    add coin

---
+++ Assets/CollectCoin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public float m_MaxLifeTime = 15f;
+    public int m_CoinValue = 100;
+    public float m_CoinRadius = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Masuk");
+        Destroy(gameObject, m_MaxLifeTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        TankCoin targetCoin = other.GetComponent<TankCoin>();
+        if (targetCoin)
+        {
+            targetCoin.TakeCoin(m_CoinValue);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/TankCoin.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankCoin : MonoBehaviour
+{
+    public LayerMask m_CoinMask;
+    public int m_StartingCoin = 0;
+    private int m_CurrentCoin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    private void OnEnable()
+    {
+        // When the tank is enabled, reset the tank's coin.
+        m_CurrentCoin = m_StartingCoin;
+
+        // Update the health slider's value and color.
+        //SetHealthUI();
+    }
+
+    public void TakeCoin(int amount)
+    {
+        // Reduce current health by the amount of damage done.
+        m_CurrentCoin += amount;
+
+        // Change the UI elements appropriately.
+        //SetHealthUI();
+    }
+
+
+    public void BuyItem(int price)
+    {
+        // Reduce current health by the amount of damage done.
+        if (m_CurrentCoin >= price)
+        {
+            m_CurrentCoin -= price;
+        }
+
+
+        // Change the UI elements appropriately.
+        //SetHealthUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
